7a7cde1db2fbbe1a22cd41e4c91a1147c1432292
Shiroiii <dima.akarawak@gmail.com>
2022-10-01 23:14:50 +0100

    NavBar Setup

---
+++ dimen_consulting/src/components/Pane/Pane.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import statBgImg from '../../../public/stat-consult.png'
+
+class Carousel extends React.Component{
+    render(){
+        return(
+            <div className={'carousel carousel-slider'}>
+            </div>
+        )
+    }
+}
+
+export default Carousel
