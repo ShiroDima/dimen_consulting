db2c5c639b02bfb5d0efd59d80dc8938d72b7671
Shiroiii <dima.akarawak@gmail.com>
2022-10-02 19:21:02 +0100

    uodated  navbar

--- dimen_consulting/src/components/Navbar/Navbar.js
+++ dimen_consulting/src/components/Navbar/Navbar.js
@@ -1,39 +1,37 @@
 import React, { useState } from 'react'
+import navLinks from '../../constants'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
 
-    const handleClick = () => {
-        setNav(!nav)
-    }
-
     return(
-        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='w-screen h-[100px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
             <div className='px-2 flex justify-between items-center w-full h-full'>
-                <div className='flex items-center'>
-                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Dimen Consulting</h1>
+                <div className='flex flex-wrap items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Dimen <br /> Consulting</h1>
                     <ul className='hidden md:flex'>
-                        <a href='/'><li>Home</li></a>
-                        <a href='/'><li>About Us</li></a>
-                        <a href='/'><li>Request Form</li></a>
-                        <a href='/'><li>Contact Us</li></a>
+                        {
+                            navLinks.map((link, idx) => {
+                                                    return <a href={`#${link.id}`} className='hover:shadow-sm hover:text-indigo-600'><li>{link.text}</li></a>
+                                                })
+                        }
                     </ul>
                 </div>
-                <div className='hidden md:flex pr-4'>
+                <div className='hidden md:flex flex-shrink-2 pr-4'>
                     <button className='border-none bg-transparent text-black mr-4'>Sign In</button>
                     <button className='px-8 py-3'>Sign Up</button>
                 </div>
-                <div className='md:hidden' onClick={handleClick}>
+                <div className='md:hidden' onClick={() => setNav((prev) => !prev)}>
                     {!nav ? <Bars3Icon className='w-5' /> : <XMarkIcon className='w-5' />}
                 </div>
             </div>
 
             <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                <li className='border-b-2 border-zinc-300 w-full'>Home</li>
-                <li className='border-b-2 border-zinc-300 w-full'>About Us</li>
-                <li className='border-b-2 border-zinc-300 w-full'>Request Form</li>
-                <li className='border-b-2 border-zinc-300 w-full'>Contact Us</li>
+                <a href='/'><li className='border-b-2 border-zinc-300 w-full'>Home</li></a>
+                <a href='/'><li className='border-b-2 border-zinc-300 w-full'>About Us</li></a>
+                <a href='/'><li className='border-b-2 border-zinc-300 w-full'>Request Form</li></a>
+                <a href='/'><li className='border-b-2 border-zinc-300 w-full'>Contact Us</li></a>
                 <div className='flex flex-col my-4'>
                     <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
                     <button className='px-8 py-3'>Sign Up</button>
---
+++ dimen_consulting/src/constants.js
@@ -0,0 +1,20 @@
+const navLinks = [
+    {
+        id: "home",
+        text: "Home"
+    },
+    {
+        id: "about",
+        text: "About Us"
+    },
+    {
+        id: "request",
+        text: "Request Form"
+    },
+    {
+        id: "contact",
+        text: "Contact Us"
+    }
+]
+
+export default navLinks
